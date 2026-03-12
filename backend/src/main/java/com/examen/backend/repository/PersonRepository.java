3b095fdf29ca9c8d02084314e937480550fc0e40
m4nul0l <manuel.seb.munoz@gmail.com>
2026-03-12 10:54:03 -0300

    remove backend folder from frontend branch

--- backend/src/main/java/com/examen/backend/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.examen.backend.repository;
-
-import com.examen.backend.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
